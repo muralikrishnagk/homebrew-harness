2cf89b8a0dce0136b57b3dd92ba883f180272db4
mkg <murali.krishna@harness.io>
2025-03-05 12:47:08 +0530

    Update formula: Use local package source

--- Formula/harness-devx.rb
+++ Formula/harness-devx.rb
@@ -1,6 +1,8 @@
 class HarnessDevx < Formula
   desc "Harness DevX Platform - Local Development Environment Setup"
   homepage "https://harness.io"
+  url "file:///Users/muralikrishnag/harness-ws/homebrew-harness/pkg/harness-devx-1.0.0.tar.gz"
+  sha256 "385ae477793cd0911996ea1c6e6e398bac7d01d1c662746c34951b9bef11d25f"
   version "1.0.0"
   license "PolyForm Free Trial 1.0.0"
 
